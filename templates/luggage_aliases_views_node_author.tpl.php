ffab5deee362416edc241858a5f5b178430a871d
rlfrahm <frahmryan@gmail.com>
2016-03-31 12:19:26 -0500

    CROPS-200 : New field format, style changes

--- templates/luggage_aliases_views_node_author.tpl.php
+++ templates/luggage_aliases_views_node_author.tpl.php
@@ -1,13 +1,13 @@
 <div class="media">
     <?php if(!$aliasName): ?>
     <div class="media-left">
-        <a href="<?php print $GLOBALS['base_url']; ?><?php if(!$aliasName) print "/" . drupal_get_path_alias('node/' . $url); else print $url; ?>">
+        <a href="<?php print $url; ?>">
             <img src="<?php print $image_src; ?>" class="img-circle img-avatar">
         </a>
     </div>
     <div class="media-body">
         <h4 class="media-heading">
-            <a href="<?php print $GLOBALS['base_url']; ?><?php if(!$aliasName) print "/" . drupal_get_path_alias('node/' . $url); else print $url; ?>"><?php print $name; ?>
+            <a href="<?php print $url; ?>"><?php print $name; ?>
             </a>
             <br />
             <small><?php print $created; ?></small>
